f71db017d02fbb184fa7ebb4c22343e12704f9a9
adrianoalex1977 <32209899+adrianoalex1977@users.noreply.github.com>
2025-04-24 19:36:51 -0300

    Atualizar o firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, set, get, push } from "firebase/database";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAUw5QkA8MWnUus5PYM-BSKTVVGL8gRVsg",
   authDomain: "construahistoria-ec81b.firebaseapp.com",
   databaseURL: "https://construahistoria-ec81b-default-rtdb.firebaseio.com",
   projectId: "construahistoria-ec81b",
-  storageBucket: "construahistoria-ec81b.firebasestorage.app",
+  storageBucket: "construahistoria-ec81b.appspot.com",
   messagingSenderId: "456242999327",
   appId: "1:456242999327:web:143ede3fbbcb37051f252a",
   measurementId: "G-XWXGWN695B"
@@ -20,6 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
-// Exportar para uso no script.js
+// Export for use in other modules
 export { database, ref, set, get, push };
